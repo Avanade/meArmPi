d1e3e1b5173d9acb6d875e345b6bb5e8b8ba19b7
Thor Schueler <thor.t.schueler@gmail.com>
2018-09-04 14:20:06 -0500

    Added meArm service for serviced

--- server_test.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/hello')
-def helloWorldHandler():
-    return 'Hello World from Flask running on the PI!'
- 
-app.run(host='127.0.0.1', port= 8090)
